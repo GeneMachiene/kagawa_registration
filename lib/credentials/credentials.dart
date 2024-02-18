c92946fd15420b4c0eded2ccb7c6afaaf49ad0c3
Gene Anthony <88444596+GeneMachiene@users.noreply.github.com>
2024-02-18 22:33:17 +0800

    Credentials

--- lib/credentials/credentials.dart
+++ lib/credentials/credentials.dart
@@ -1,10 +1,10 @@
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:kagawa_registration/credentials/credentials.dart';
+import 'package:kagawa_registration/credentials/add.dart';
 
-class Description extends StatelessWidget {
-  Description({super.key});
+class Credentials extends StatelessWidget {
+  Credentials({super.key});
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -23,11 +23,6 @@ class Description extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Image.asset(
-            'assets/Folder.png',
-            width: double.infinity,
-            fit: BoxFit.fitWidth,
-          ),
           Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -35,7 +30,7 @@ class Description extends StatelessWidget {
                 Column(
                   children: [
                     Text(
-                      'Tell us about yourself',
+                      'Add some Credentials',
                       style: GoogleFonts.lexendDeca(
                           textStyle: theme.textTheme.headlineMedium,
                           fontWeight: FontWeight.w700
@@ -61,39 +56,46 @@ class Description extends StatelessWidget {
                     ),
                   ],
                 ),
-
                 FormBuilder(
                   key: _formKey,
                   child: Padding(
                     padding: const EdgeInsets.all(15),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: FormBuilderTextField(
-                        name: 'description',
-                        style: theme.textTheme.titleMedium,
-                        maxLines: 10,
-                        minLines: 10,
-                        decoration: const InputDecoration(
-                          labelText: 'Description',
-                          alignLabelWithHint: true,
-                          border: OutlineInputBorder(),
-                          hintText: "Ex. I specialize in tackling "
-                              "everything from leaky faucets to "
-                              "intricate pipe installations. I take "
-                              "pride in my meticulous attention to "
-                              "detail and commitment to delivering "
-                              "top-notch service to every client. "
-                              "Whether it\'s a residential repair or a "
-                              "commercial project, you can trust me to "
-                              "arrive promptly, diagnose the issue "
-                              "efficiently, and provide cost-effective "
-                              "solutions tailored to your needs.",
+                    child: Column(
+                      children: [
+                        Text('Add some Credentials to increase trust.'),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: Card(
+                            child: Text('Sample Credential Card')
+                          ),
                         ),
-                      ),
-                    ),
+                        Padding(
+                          padding: const EdgeInsets.all(50),
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                width: double.infinity,
+                                child: ElevatedButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(builder: (context) => AddCred()),
+                                      );
+                                    },
+                                    child: const Padding(
+                                      padding: EdgeInsets.all(15),
+                                      child: Icon(Icons.add),
+                                    )
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    )
                   ),
                 ),
-
                 Padding(
                   padding: const EdgeInsets.all(50),
                   child: Column(
@@ -104,7 +106,7 @@ class Description extends StatelessWidget {
                             onPressed: () {
                               Navigator.push(
                                 context,
-                                MaterialPageRoute(builder: (context) => Credentials()),
+                                MaterialPageRoute(builder: (context) => const Placeholder()),
                               );
                             },
                             child: const Padding(
@@ -119,7 +121,7 @@ class Description extends StatelessWidget {
                             onPressed: () {
                               Navigator.push(
                                 context,
-                                MaterialPageRoute(builder: (context) => Credentials()),
+                                MaterialPageRoute(builder: (context) => const Placeholder()),
                               );
                             },
                             child: const Padding(
